43677748309a0a1f5715e03177892e88ee5d7ffd
vrecan <vrecan@localhost.localdomain>
2015-02-11 15:24:06 -0700

    fix the service and log location

--- rift.go
+++ rift.go
@@ -3,12 +3,19 @@ package main
 import (
 	log "github.com/cihub/seelog"
 	"github.com/vrecan/rift/start"
+        "runtime"
 )
 
 func main() {
 
 	defer log.Flush()
-	logger, err := log.LoggerFromConfigAsFile("seelog.xml")
+	var err error
+        var logger log.LoggerInterface
+	if(runtime.GOOS == "windows") {
+	logger, err = log.LoggerFromConfigAsFile("seelog_windows.xml")
+	} else {
+	logger, err = log.LoggerFromConfigAsFile("seelog.xml")
+ 	}
 
 	if err != nil {
 		log.Warn("Failed to load config", err)
